d679cd869bd7abe38135b2bef8f0b23f889e498c
PedroGregolini <pedrogregolini@gmail.com>
2025-03-23 23:48:04 -0300

    Corrigindo o Codigo

--- script.js
+++ script.js
@@ -7,95 +7,51 @@ var resultado = document.querySelector(".texto-resultado");
 botaoEncriptar.onclick = encriptar;
 botaoDesencriptar.onclick = desencriptar;
 
-function encriptar(){
+function encriptar() {
     ocultar();
-    var caixatexto = recuperarTexto()
-    resultado.textContent = encriptarTexto(caixatexto);
+    var texto = recuperarTexto();
+    resultado.textContent = encriptarTexto(texto);
+    resultado.classList.remove("ocultar");
 }
 
-function desencriptar(){
+function desencriptar() {
     ocultar();
-    var caixatexto = recuperarTexto()
-    resultado.textContent = desencriptarTexto(caixatexto);
+    var texto = recuperarTexto();
+    resultado.textContent = desencriptarTexto(texto);
+    resultado.classList.remove("ocultar");
 }
 
-function recuperarTexto(){
-    var caixatexto = document.querySelector(".caixatexto")
-    return caixatexto.value
+function recuperarTexto() {
+    return document.querySelector(".caixatexto").value;
 }
 
-function ocultar(){
+function ocultar() {
     hominho.classList.add("ocultar");
     paragrafo.classList.add("ocultar");
 }
 
-function encriptarTexto(mensagem) {
-    var texto = mensagem;
-    var textoFinal = "";
-
-    for(var i = 0; i < texto.length; i++){
-        if(texto[i] == "a"){
-            textoFinal = textoFinal + "ai"
-        }
-        else if(texto[i] == "e"){
-            textoFinal = textoFinal + "enter"
-        }
-        else if(texto[i] == "i"){
-            textoFinal = textoFinal + "imes"
-        }
-        else if(texto[i] == "o"){
-            textoFinal = textoFinal + "ober"
-        }
-        else if(texto[i] == "u"){
-            textoFinal = textoFinal + "ufat"
-        }
-        else{
-            textoFinal = textoFinal + texto[i]
-        }
-    }
-    return textoFinal;
-
+function encriptarTexto(texto) {
+    return texto
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
 }
 
-function desencriptarTexto(mensagem){
-    var texto = mensagem;
-    var textoFinal = "";
-
-    for(var i = 0; i < texto.length; i++){
-        if(texto[i] == "a"){
-            textoFinal = textoFinal + "a"
-            i = i+1;
-        }
-        else if(texto[i] == "e"){
-            textoFinal = textoFinal + "e"
-            i = i+4;
-        }
-        else if(texto[i] == "i"){
-            textoFinal = textoFinal + "i"
-            i = i+3;
-        }
-        else if(texto[i] == "o"){
-            textoFinal = textoFinal + "o"
-            i = i+3;
-        }
-        
-        else if(texto[i] == "u"){
-            textoFinal = textoFinal + "u"
-            i = i+3;
-        }
-        else{
-            textoFinal = textoFinal + texto[i];
-        }
-        
-    }
-
-    return textoFinal;
-
+function desencriptarTexto(texto) {
+    return texto
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
 }
 
-const btnCopiar = document.querySelector(".btn-copiar"); 
-    btnCopiar.addEventListener("click", copiar = () => {
-    var contenido = document.querySelector(".texto-resultado").textContent;
-    navigator.clipboard.writeText(contenido);
-    console.log("ola"); 
+const btnCopiar = document.querySelector(".btn-copiar");
+btnCopiar.addEventListener("click", () => {
+    var textoCopiar = resultado.textContent;
+    navigator.clipboard.writeText(textoCopiar).then(() => {
+        alert("Texto copiado com sucesso!");
+    });
 });
